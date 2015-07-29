2f9c789cbe65926addd79314e218d43a1ea9c368
pankona <yosuke.akatsuka@gmail.com>
2015-07-29 20:03:40 +0900

    initial commit. still WIP.

---
+++ mymine.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"menteslibres.net/gosexy/rest"
+	"os"
+	"strings"
+)
+
+func getEnvVar(varName string) (result string) {
+	for _, e := range os.Environ() {
+		pair := strings.Split(e, "=")
+		if pair[0] == varName {
+			return pair[1]
+		}
+	}
+	return ""
+}
+
+func main() {
+	redmineUrl := getEnvVar("REDMINE_URL")
+	if redmineUrl == "" {
+		fmt.Println("REDMINE_URL is not specified.")
+		return
+	}
+
+	redmineApiKey := getEnvVar("REDMINE_API_KEY")
+	if redmineApiKey == "" {
+		fmt.Println("REDMINE_API_KEY is not specified.")
+		return
+	}
+
+	var buf map[string]interface{}
+	rest.Get(&buf, redmineUrl+"issues.json?key="+redmineApiKey+"&status_id=open&assigned_to_id=me", nil)
+}
